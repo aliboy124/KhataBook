dbd2219cf0c8623420ccb3f392c344d536c2e328
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2022-01-19 23:38:30 +0500

    configure firebase authentication

--- app/src/main/java/com/example/khatabook/Signup.java
+++ app/src/main/java/com/example/khatabook/Signup.java
@@ -4,11 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class SignUp extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding = ActivitySignUpBinding.inflate((getLayoutInflater()));
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        auth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword(
+                        binding.edEmail.getText().toString(),
+                        binding.edPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Users user = new Users(
+                                            binding.edUser.getText().toString(),
+                                            binding.edEmail.getText().toString(),
+                                            binding.edPassword.getText().toString()
+                                    );
+                                    String id = task.getResult().getUser().getUid();
+                                    database.getReference().child("Users").child(id).setValue(user);
+
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
