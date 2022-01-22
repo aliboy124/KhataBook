274ea46124492717227e57f876e02d144588f078
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2022-01-23 04:09:20 +0500

    Update NewTransaction.java

--- app/src/main/java/com/example/khatabook/NewTransaction.java
+++ app/src/main/java/com/example/khatabook/NewTransaction.java
@@ -1,5 +1,6 @@
 package com.example.khatabook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class NewTransaction extends AppCompatActivity {
 
@@ -22,6 +34,7 @@ public class NewTransaction extends AppCompatActivity {
 
 
         User user = new User();
+        User userReceiver = new User();
 
         Bundle extras = getIntent().getExtras();
 
@@ -41,7 +54,52 @@ public class NewTransaction extends AppCompatActivity {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
-                    
+
+                    if (!receiver.getText().toString().equals(""))
+                    {
+                        if(receiver.getText().toString().equals(user.getEmail()))
+                        {
+                            Toast.makeText(getApplicationContext(), "Sender and receiver cant be same!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        DatabaseReference dbreference = FirebaseDatabase.getInstance().getReference();
+//                        dbreference = dbreference.child("Users");
+
+                        dbreference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                if (snapshot.exists()){
+                                    HashMap<String, Object> dataMap = (HashMap<String, Object>) snapshot.getValue();
+
+                                    for (String key : dataMap.keySet()){
+
+                                        Object data = dataMap.get(key);
+                                        HashMap<String, Object> userData = (HashMap<String, Object>) data;
+
+                                        if (userData.get("email").equals(receiver.getText().toString()))
+                                        {
+                                            userReceiver.setEmail(userData.get("email").toString());
+                                            userReceiver.setName(userData.get("name").toString());
+                                            userReceiver.setPassword(userData.get("name").toString());
+                                            userReceiver.setNegBalance(Integer.parseInt(userData.get("negBalance").toString()));
+                                            userReceiver.setPosBalance(Integer.parseInt(userData.get("posBalance").toString()));
+                                            break;
+                                        }
+                                    }
+
+
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                    }
                 }
             }
         });
