f228f93523b51cffeadea8ebdbc9469dd3ad69b2
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2022-01-23 02:28:11 +0500

    new transaction implementation

--- app/src/main/java/com/example/khatabook/NewTransaction.java
+++ app/src/main/java/com/example/khatabook/NewTransaction.java
@@ -3,6 +3,9 @@ package com.example.khatabook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewTransaction extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class NewTransaction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_transaction);
+
+        Button submit = findViewById(R.id.submit);
+        EditText sender = findViewById(R.id.senderr);
+        EditText receiver = findViewById(R.id.receiver);
+        EditText amount = findViewById(R.id.amountt);
+
+
+
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+            }
+        });
+
+
     }
 }
