9f28e62980360e90167381f87307037d3b2685aa
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2022-01-23 04:52:27 +0500

    store data from firebase to datasnapshot

--- app/src/main/java/com/example/khatabook/NewTransaction.java
+++ app/src/main/java/com/example/khatabook/NewTransaction.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,9 +20,16 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class NewTransaction extends AppCompatActivity {
+
+
+    public static <K, V> Map<K, V> copyMap(Map<K, V> original)
+    {
+        return new HashMap<>(original);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +45,66 @@ public class NewTransaction extends AppCompatActivity {
         User user = new User();
         User userReceiver = new User();
 
-        Bundle extras = getIntent().getExtras();
 
-        if (extras != null) {
-            user.setName(extras.getString("name"));
-            user.setEmail(extras.getString("email"));
-            user.setPosBalance(extras.getInt("pos"));
-            user.setNegBalance(extras.getInt("neg"));
-            user.setPassword(extras.getString("password"));
+        final Bundle[] extras = {getIntent().getExtras()};
+
+        if (extras[0] != null) {
+            user.setName(extras[0].getString("name"));
+            user.setEmail(extras[0].getString("email"));
+            user.setPosBalance(extras[0].getInt("pos"));
+            user.setNegBalance(extras[0].getInt("neg"));
+            user.setPassword(extras[0].getString("password"));
 
             sender.setText(user.getEmail());
             sender.setEnabled(false);
         }
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        final DataSnapshot dataSnapshot;
+
+        DatabaseReference dbreference = FirebaseDatabase.getInstance().getReference();
+        dbreference = dbreference.child("Users");
+
+        dbreference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                 dataSnapshot = snapshot;
+
+                if (snapshot.exists()){
+                    HashMap<String, Object> dataMap = (HashMap<String, Object>) snapshot.getValue();
+
+                    for (String key : dataMap.keySet()){
+
+                        Object data = dataMap.get(key);
+                        HashMap<String, Object> userData = (HashMap<String, Object>) data;
+
+                        String temp = userData.get("email").toString();
+                        if ( temp == receiver.getText().toString())
+                        {
+                            userReceiver.setEmail(userData.get("email").toString());
+                            userReceiver.setName(userData.get("name").toString());
+                            userReceiver.setPassword(userData.get("name").toString());
+                            userReceiver.setNegBalance(Integer.parseInt(userData.get("negBalance").toString()));
+                            userReceiver.setPosBalance(Integer.parseInt(userData.get("posBalance").toString()));
+                            break;
+                        }
+                    }
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
 
 
         receiver.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -63,42 +120,6 @@ public class NewTransaction extends AppCompatActivity {
                             return;
                         }
 
-                        DatabaseReference dbreference = FirebaseDatabase.getInstance().getReference();
-//                        dbreference = dbreference.child("Users");
-
-                        dbreference.addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                                if (snapshot.exists()){
-                                    HashMap<String, Object> dataMap = (HashMap<String, Object>) snapshot.getValue();
-
-                                    for (String key : dataMap.keySet()){
-
-                                        Object data = dataMap.get(key);
-                                        HashMap<String, Object> userData = (HashMap<String, Object>) data;
-
-                                        if (userData.get("email").equals(receiver.getText().toString()))
-                                        {
-                                            userReceiver.setEmail(userData.get("email").toString());
-                                            userReceiver.setName(userData.get("name").toString());
-                                            userReceiver.setPassword(userData.get("name").toString());
-                                            userReceiver.setNegBalance(Integer.parseInt(userData.get("negBalance").toString()));
-                                            userReceiver.setPosBalance(Integer.parseInt(userData.get("posBalance").toString()));
-                                            break;
-                                        }
-                                    }
-
-
-
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
-                        });
                     }
                 }
             }
