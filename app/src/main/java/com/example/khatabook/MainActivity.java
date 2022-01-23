6e7bedbf985bb3c6dc970783041141d17c41f7ec
aliboy124 <aligreat124@gmail.com>
2022-01-24 00:33:14 +0500

    Added onClickListener and Dialog box

--- app/src/main/java/com/example/khatabook/MainActivity.java
+++ app/src/main/java/com/example/khatabook/MainActivity.java
@@ -10,9 +10,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -209,8 +211,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void setTransactionListAdapter(){
         recyclerView = findViewById(R.id.recyclerView);
-
-
 
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
