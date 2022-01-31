3bd886f9ca7b843f253193b56689d275b6692644
aliboy124 <aligreat124@gmail.com>
2022-01-31 21:41:58 +0500

    Splash Screen Added

---
+++ app/src/main/java/com/example/khatabook/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.khatabook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,Signup.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+    }
+}
