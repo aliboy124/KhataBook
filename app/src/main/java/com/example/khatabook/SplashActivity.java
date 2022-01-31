5447462fbb55d00d51ae0d6f626c70ced867e163
aliboy124 <aligreat124@gmail.com>
2022-01-31 21:57:59 +0500

    Splash Screen Animations added and signed apk generated

--- app/src/main/java/com/example/khatabook/SplashActivity.java
+++ app/src/main/java/com/example/khatabook/SplashActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 
@@ -12,6 +16,16 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashfile);
+
+        ImageView image = (ImageView)findViewById(R.id.splashimageview);
+        Animation anim1 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_left);
+        image.startAnimation(anim1);
+
+        TextView txt = (TextView)findViewById(R.id.splashtextview);
+        Animation anim2 = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_right);
+        txt.startAnimation(anim2);
 
         handler=new Handler();
         handler.postDelayed(new Runnable() {
